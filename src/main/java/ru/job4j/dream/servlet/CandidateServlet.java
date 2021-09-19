dd8817c8bd53fe7b52997987041a6d20debcad81
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-09-19 22:50:52 +0300

    3. Редактирование вакансии. [#277566]

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -1,19 +1,19 @@
 package ru.job4j.dream.servlet;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import ru.job4j.dream.model.Candidate;
-import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.Store;
 
 public class CandidateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().saveCandidate(new Candidate(0, req.getParameter("name")));
+        Store.instOf().saveCandidate(new Candidate(
+                Integer.parseInt(req.getParameter("id")),
+                req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
     }
 }
--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -1,6 +1,5 @@
 package ru.job4j.dream.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,9 @@ public class PostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().savePost(new Post(0, req.getParameter("name")));
+        Store.instOf().savePost(new Post(
+                Integer.parseInt(req.getParameter("id")),
+                req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/posts.jsp");
     }
 }
