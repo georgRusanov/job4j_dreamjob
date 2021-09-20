26ec5c7764838f1b939a1656aecedae0107a9889
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-09-20 16:40:11 +0300

    0. MVC в Servlet. [#6868]

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -1,6 +1,7 @@
 package ru.job4j.dream.servlet;
 
 import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +10,17 @@ import ru.job4j.dream.store.Store;
 
 public class CandidateServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("candidates", Store.instOf().findAllCandidates());
+        req.getRequestDispatcher("candidates.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().saveCandidate(new Candidate(
                 Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")));
-        resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
+        resp.sendRedirect(req.getContextPath() + "/candidates.do");
     }
 }
--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +10,17 @@ import ru.job4j.dream.store.Store;
 
 public class PostServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.getRequestDispatcher("posts.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().savePost(new Post(
                 Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")));
-        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+        resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 }
