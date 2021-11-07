0a69e1e283eb2509f54365aa299ea3e1cd4528d0
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2021-11-07 04:30:17 +0300

    task 281226

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -6,19 +6,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import ru.job4j.dream.model.Post;
-import ru.job4j.dream.store.Store;
+import ru.job4j.dream.store.MemStore;
 
 public class PostServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.setAttribute("posts", MemStore.instOf().findAllPosts());
         req.getRequestDispatcher("posts.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().savePost(new Post(
+        MemStore.instOf().savePost(new Post(
                 Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/posts.do");
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,61 +1,16 @@
 package ru.job4j.dream.store;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class Store {
+public interface Store {
+    Collection<Post> findAllPosts();
 
-    private static final Store INST = new Store();
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-    private static AtomicInteger POST_ID = new AtomicInteger(4);
-    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+    Collection<Candidate> findAllCandidates();
 
-    private Store() {
-        posts.put(1, new Post(1, "Junior Java Job", "first"));
-        posts.put(2, new Post(2, "Middle Java Job", "second"));
-        posts.put(3, new Post(3, "Senior Java Job", "third"));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
-    }
+    void save(Post post);
 
-    public static Store instOf() {
-        return INST;
-    }
-
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
-
-    public Post findPostById(int id) {
-        return posts.get(id);
-    }
-
-    public Candidate findCandidateById(int id) {
-        return candidates.get(id);
-    }
-
-    public void savePost(Post post) {
-        if (post.getId() == 0) {
-            post.setId(POST_ID.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
-
-    public void saveCandidate(Candidate candidate) {
-        if (candidate.getId() == 0) {
-            candidate.setId(CANDIDATE_ID.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
+    Post findById(int id);
 }
