e8f7980f28093d12d63989be30e507e13ebbf317
georgiy.rusanov <georgiy.rusanov@nordigy.ru>
2022-01-05 23:30:43 +0300

    fixed dbstore tests

--- src/test/java/ru/job4j/dream/store/DbStoreTest.java
+++ src/test/java/ru/job4j/dream/store/DbStoreTest.java
@@ -6,14 +6,15 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.Test;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
-import org.junit.Test;
 
 public class DbStoreTest {
-    private static final Store store = DbStore.instOf();
+
     @Test
     public void whenCreatePost() {
+        Store store = DbStore.instOf();
         Post post = new Post(0, "Java Job0");
         store.savePost(post);
         Post postInDb = store.findPostById(post.getId());
@@ -22,6 +23,7 @@ public class DbStoreTest {
 
     @Test
     public void whenCreateCandidate() {
+        Store store = DbStore.instOf();
         Candidate candidate = new Candidate(0, "Name0");
         store.saveCandidate(candidate);
         Candidate candidateInDb = store.findCandidateById(candidate.getId());
@@ -30,15 +32,17 @@ public class DbStoreTest {
 
     @Test
     public void testInstOf() {
+        Store store = DbStore.instOf();
         Store store1 = DbStore.instOf();
         assertEquals(store1, store);
     }
 
     @Test
     public void testFindAllPosts() {
-        Post post = new Post(2, "Java Job2");
+        Store store = DbStore.instOf();
+        Post post = new Post(0, "Java Job2");
         store.savePost(post);
-        Post post2 = new Post(3, "Java Job3");
+        Post post2 = new Post(0, "Java Job3");
         store.savePost(post2);
         ArrayList<Post> posts = (ArrayList<Post>) store.findAllPosts();
         ArrayList<Post> expectedPosts = new ArrayList<>(List.of(post, post2));
@@ -47,9 +51,10 @@ public class DbStoreTest {
 
     @Test
     public void testFindAllCandidates() {
-        Candidate candidate = new Candidate(2, "Name2");
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Name2");
         store.saveCandidate(candidate);
-        Candidate candidate2 = new Candidate(3, "Name3");
+        Candidate candidate2 = new Candidate(0, "Name3");
         store.saveCandidate(candidate2);
         ArrayList<Candidate> candidates =  (ArrayList<Candidate>) store.findAllCandidates();
         ArrayList<Candidate> expectedCandidates = new ArrayList<>(List.of(candidate, candidate2));
@@ -58,11 +63,11 @@ public class DbStoreTest {
 
     @Test
     public void testDeleteCandidate() {
-        Candidate candidate = new Candidate(5, "Name5");
+        Store store = DbStore.instOf();
+        Candidate candidate = new Candidate(0, "Name5");
         store.saveCandidate(candidate);
         store.deleteCandidate(candidate.getId());
         Candidate actual = store.findCandidateById(candidate.getId());
         assertNull(actual);
-
     }
 }
